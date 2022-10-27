202df7ab26172e8202d45d813981e682002f7bd9
Moonode <55302867+Luna-Moonode@users.noreply.github.com>
2022-10-27 09:50:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ client = WeChatClient(app_id, app_secret)
 wm = WeChatMessage(client)
 wea_you, temperature_you = get_weather(city_you)
 
-words = "来啦来啦宝，嘿嘿"
+words = get_words()
 #发送的数据
 data = {
   "weather":{"value":wea_you},
